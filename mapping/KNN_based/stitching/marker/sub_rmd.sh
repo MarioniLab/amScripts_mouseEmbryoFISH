25b81262cec5c0b291050e625bc366d6236792b5
amissarova <alsu.missarova.32@gmail.com>
2020-03-22 08:55:26 +0000

    par

--- mapping/KNN_based/stitching/marker/sub_rmd.sh
+++ mapping/KNN_based/stitching/marker/sub_rmd.sh
@@ -9,10 +9,10 @@ script_name=mar_bp
 
 #CHOOSE PARAMETERS
 #RAM in megabytes
-memory=200000
+memory=250000
 r_command="rusage[mem=${memory}]"
 #num_processors
-nproc=10
+nproc=5
 
 smg=/nfs/research1/marioni/alsu/singularity/R1.simg
 script=/nfs/research1/marioni/alsu/spatial/mouse_embryo/amScripts_mouseEmbryoFISH/mapping/KNN_based/stitching/marker/run_rmd.R
--- mapping/KNN_based/stitching/pca/sub_rmd.sh
+++ mapping/KNN_based/stitching/pca/sub_rmd.sh
@@ -5,14 +5,14 @@ err_folder=${my_folder}/clust_err/spatial
 
 #SELECT SCRIPT
 #If you change this, you MUST update the wrapper's grep
-script_name=pca_est
+script_name=pca_bp
 
 #CHOOSE PARAMETERS
 #RAM in megabytes
-memory=200000
+memory=250000
 r_command="rusage[mem=${memory}]"
 #num_processors
-nproc=10
+nproc=5
 
 smg=/nfs/research1/marioni/alsu/singularity/R1.simg
 script=/nfs/research1/marioni/alsu/spatial/mouse_embryo/amScripts_mouseEmbryoFISH/mapping/KNN_based/stitching/pca/run_rmd.R
