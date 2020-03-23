277b7cc763f8efe3df52ffdc228deaa0593a19df
amissarova <alsu.missarova.32@gmail.com>
2020-03-23 13:21:11 +0000

    corrected

---
+++ mapping/KNN_based/stitching/pca_mean/run_rmd.R
@@ -0,0 +1,3 @@
+library(rmarkdown)
+
+render("/nfs/research1/marioni/alsu/spatial/mouse_embryo/amScripts_mouseEmbryoFISH/mapping/KNN_based/stitching/pca_mean/stitching_pca_mean.Rmd")
---
+++ mapping/KNN_based/stitching/pca_weightedMean/sub_rmd.sh
@@ -0,0 +1,23 @@
+#INITIALISE FOLDERS
+my_folder=/nfs/research1/marioni/alsu
+out_folder=${my_folder}/clust_out/spatial
+err_folder=${my_folder}/clust_err/spatial
+
+#SELECT SCRIPT
+#If you change this, you MUST update the wrapper's grep
+script_name=pca_wei
+
+#CHOOSE PARAMETERS
+#RAM in megabytes
+memory=200000
+r_command="rusage[mem=${memory}]"
+#num_processors
+nproc=5
+
+smg=/nfs/research1/marioni/alsu/singularity/R1.simg
+script=/nfs/research1/marioni/alsu/spatial/mouse_embryo/amScripts_mouseEmbryoFISH/mapping/KNN_based/stitching/pca_weightedMean/run_rmd.R
+
+bsub -q research-rh74 -e ${err_folder}/${script_name} \
+-o ${out_folder}/${script_name} \
+-M $memory -R $r_command -n $nproc -J ${script_name} \
+"singularity exec $smg Rscript $script"
