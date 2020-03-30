2e143dfbcf29dc78c36f24d5f79b9f04710e87e1
amissarova <alsu.missarova.32@gmail.com>
2020-03-30 11:14:27 +0100

    em 3

---
+++ mapping/KNN_based/stitching/pca_mean_embryo3/run_rmd.R
@@ -0,0 +1,3 @@
+library(rmarkdown)
+
+render("/nfs/research1/marioni/alsu/spatial/mouse_embryo/amScripts_mouseEmbryoFISH/mapping/KNN_based/stitching/pca_mean_embryo3/stitching_pca_mean_embryo3.Rmd")
