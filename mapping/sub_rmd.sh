022c9a25a2257226287796b32c5bf554a74228e0
amissarova <alsu.missarova.32@gmail.com>
2020-04-26 16:18:44 +0100

    less mem

--- mapping/sub_rmd.sh
+++ mapping/sub_rmd.sh
@@ -9,10 +9,10 @@ script_name=pca
 
 #CHOOSE PARAMETERS
 #RAM in megabytes
-memory=100000
+memory=90000
 r_command="rusage[mem=${memory}]"
 #num_processors
-nproc=3
+nproc=2
 
 smg=/nfs/research1/marioni/alsu/singularity/R1.simg
 script=/nfs/research1/marioni/alsu/spatial/mouse_embryo/amScripts_mouseEmbryoFISH/mapping/run_rmd.R
