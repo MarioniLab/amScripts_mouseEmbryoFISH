388f401613afb19328ac9825e59f7f5e54cdc4de
amissarova <alsu.missarova.32@gmail.com>
2020-05-08 13:44:47 +0100

    fix

--- analysis/CT2CT/CT2CT__markers/run_rmd.R
+++ analysis/CT2CT/CT2CT__markers/run_rmd.R
@@ -1,3 +1,3 @@
 library(rmarkdown)
 
-render("/nfs/research1/marioni/alsu/spatial/mouse_embryo/amScripts_mouseEmbryoFISH/analysis/CT2CT/CT2CT__markers/run_rmd.R")
+render("/nfs/research1/marioni/alsu/spatial/mouse_embryo/amScripts_mouseEmbryoFISH/analysis/CT2CT/CT2CT__markers/CT2CT__markers.Rmd")
