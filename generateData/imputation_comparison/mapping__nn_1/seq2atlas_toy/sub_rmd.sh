e0c2546aae31cb5a36a93b097eca93f5bc0e85e0
amissarova <alsu.missarova.32@gmail.com>
2020-06-08 16:54:29 +0100

    toy seq2atlas

--- generateData/imputation_comparison/mapping__nn_1/seq2atlas_toy/sub_rmd.sh
+++ generateData/imputation_comparison/mapping__nn_1/seq2atlas_toy/sub_rmd.sh
@@ -9,10 +9,10 @@ script_name=seq_nn1_toy
 
 #CHOOSE PARAMETERS
 #RAM in megabytes
-memory=95000
+memory=49000
 r_command="rusage[mem=${memory}]"
 #num_processors
-nproc=10
+nproc=5
 
 smg=/nfs/research1/marioni/alsu/singularity/R1.simg
 script=/nfs/research1/marioni/alsu/spatial/mouse_embryo/amScripts_mouseEmbryoFISH/generateData/imputation_comparison/mapping__nn_1/seq2atlas_toy/run_rmd.R
