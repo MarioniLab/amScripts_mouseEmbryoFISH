fb0c6d7ea3e1eb2ead7be327b4add6b98a7668a5
amissarova <alsu.missarova.32@gmail.com>
2020-06-08 17:11:15 +0100

    atlas2atlas

---
+++ generateData/imputation_comparison/mapping__nn_1/atlas2atlas/run_rmd.R
@@ -0,0 +1,3 @@
+library(rmarkdown)
+
+render("/nfs/research1/marioni/alsu/spatial/mouse_embryo/amScripts_mouseEmbryoFISH/generateData/imputation_comparison/mapping__nn_1/atlas2atlas/mapping__atlas2atlas__nn_1.Rmd")
