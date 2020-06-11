bbfecb68bc94daa050673cbf776e91cb373a407b
amissarova <alsu.missarova.32@gmail.com>
2020-06-11 22:56:36 +0100

    map

--- core_functions.R
+++ core_functions.R
@@ -10,7 +10,7 @@ load_embryo_8.5 = function(dir = "local"){
     data.dir = "/Users/alsu/Develop/spatial/mouse_embryo/data/8_5/source/"
   }
   
-  sce = readRDS( paste0( data.dir , "E8.5_sce_filt.Rds"))
+  sce = readRDS( paste0( data.dir , "E8.5_sce_filt_unlabelled.Rds"))
   meta = colData(sce)
   meta = data.frame(meta)
   
@@ -19,52 +19,6 @@ load_embryo_8.5 = function(dir = "local"){
   rownames.sce[rownames.sce == "Cavin3"] = "Prkcdbp"
   rownames(sce) = rownames.sce
   
-  # # add libsize
-  # #counts = assay(sce, "counts")
-  # #libsize = colSums( counts )
-  # #meta = cbind(meta, libsize)
-  # # to sce: add log2 normalization by cell area and libsize
-  # sizeFactors.libsize <- meta$libsize/mean(meta$libsize)
-  # sizeFactors.area <- meta$Area/mean(meta$Area)
-  # assay(sce, "logcounts.libsize") <- log2(t(t(counts)/sizeFactors.libsize) + 1)
-  # assay(sce, "logcounts.area") <- log2(t(t(counts)/sizeFactors.area) + 1)
-  # 
-  # 
-  # # filter big clumps (too big of the area)
-  # if (filterBigClumps){
-  #   meta$sqrt.area = sqrt(meta$Area)
-  #   pnorm.fit = pnorm(meta$sqrt.area, mean = median(meta$sqrt.area), 
-  #                     sd = mad(meta$sqrt.area) , lower.tail = F)
-  #   thresh.sqrt = min(meta$sqrt.area[which(p.adjust(pnorm.fit, method = "fdr") < 0.05)])
-  #   print( paste0( "Discarded ", sum(meta$sqrt.area > thresh.sqrt), " big clumps (out of ", 
-  #                  dim(meta)[1], "): ", round( mean(meta$sqrt.area > thresh.sqrt)*100, 2), "%" ))
-  #   idx = meta$sqrt.area <= thresh.sqrt
-  #   meta = meta[idx,]
-  #   sce = sce[,idx]
-  # }
-  # 
-  # # keep z-stacks we want
-  # idx = meta$z %in% zToKeep
-  # print( paste0( "Discarded ", sum(!meta$z %in% zToKeep), " from not relevant z-stacks (out of ", 
-  #                dim(meta)[1], "): ", round( mean(!meta$z %in% zToKeep)*100, 2), "%" ))
-  # meta = meta[idx, ]
-  # sce = sce[, idx]
-  # 
-  # # filter out cells with low number of mRNA molecules
-  # idx = meta$libsize > threshTotalRNA
-  # print( paste0( "Discarded ", sum(meta$libsize <= threshTotalRNA), " cells with low number of RNA molecules (out of ", 
-  #                dim(meta)[1], "): ", round( mean(meta$libsize <= threshTotalRNA)*100, 2), "%" ))
-  # meta = meta[idx,]
-  # sce = sce[, idx]
-  # 
-  # # filter out cells with low number of genes
-  # n.genes = getNumGenes(sce)
-  # idx = n.genes > threshTotalGenes 
-  # print( paste0( "Discarded ", sum(n.genes <= threshTotalGenes ), " cells with low number of genes (out of ", 
-  #                dim(meta)[1], "): ", round( mean(n.genes <= threshTotalGenes )*100, 2), "%" ))
-  # meta = meta[idx,]
-  # sce = sce[, idx]
-  # 
   assign("sce", sce, envir = .GlobalEnv)
   assign("meta", meta, envir = .GlobalEnv)
   invisible(0)
