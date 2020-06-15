843248d45a0278744df8f55eb7365f6dbd020070
amissarova <alsu.missarova.32@gmail.com>
2020-06-15 20:08:32 +0100

    one saample

---
+++ generateData/imputation/mapping/atlas2atlas_s29/run_rmd.R
@@ -0,0 +1,3 @@
+library(rmarkdown)
+
+render("/nfs/research1/marioni/alsu/spatial/mouse_embryo/amScripts_mouseEmbryoFISH/generateData/imputation/mapping/atlas2atlas_s29/mapping_atlas2atlas_s29.Rmd")
---
+++ generateData/imputation/mapping/seq2atlas_s29/run_rmd.R
@@ -0,0 +1,3 @@
+library(rmarkdown)
+
+render("/nfs/research1/marioni/alsu/spatial/mouse_embryo/amScripts_mouseEmbryoFISH/generateData/imputation/mapping/seq2atlas_s29/mapping_seq2atlas_s29.Rmd")
