3da257d2dd7badd70358f7d8ed3f1db8b221379f
amissarova <alsu.missarova.32@gmail.com>
2020-06-17 06:30:34 +0100

    perf

---
+++ generateData/imputation/performance/seq2atlas/embryo3_z5/run_rmd.R
@@ -0,0 +1,3 @@
+library(rmarkdown)
+
+render("/nfs/research1/marioni/alsu/spatial/mouse_embryo/amScripts_mouseEmbryoFISH/generateData/imputation/performance/seq2atlas/embryo3_z5/performance_seq2atlas.Rmd")
