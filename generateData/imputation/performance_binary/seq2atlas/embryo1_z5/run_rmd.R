39434921d44e727673d41dc34619c60bd1d302d4
amissarova <alsu.missarova.32@gmail.com>
2020-06-25 02:51:28 +0100

    z5

---
+++ generateData/imputation/performance_binary/seq2atlas/embryo1_z5/run_rmd.R
@@ -0,0 +1,3 @@
+library(rmarkdown)
+
+render("/nfs/research1/marioni/alsu/spatial/mouse_embryo/amScripts_mouseEmbryoFISH/generateData/imputation/performance_binary/seq2atlas/embryo1_z5/performance_seq2atlas.Rmd")
