21eb679bb48b456c4f8065d4068d3ff952c2c693
amissarova <alsu.missarova.32@gmail.com>
2020-06-28 20:44:14 +0100

    boom

---
+++ generateData/imputation/comparison_imputation_metrics/seq2atlas/embryo1_z2/run_rmd.R
@@ -0,0 +1,3 @@
+library(rmarkdown)
+
+render("/nfs/research1/marioni/alsu/spatial/mouse_embryo/amScripts_mouseEmbryoFISH/generateData/imputation/comparison_imputation_metrics/seq2atlas/embryo1_z2/comp_imputation_metrics.Rmd")
