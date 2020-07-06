f0f89ec08d06a69cc7aec11a0793cf3ec03445b0
amissarova <alsu.missarova.32@gmail.com>
2020-07-06 15:40:46 +0100

    perf + spatial coherence for seq

--- generateData/imputation/performance/seq2atlas/sub_rmd.sh
+++ generateData/imputation/performance/seq2atlas/sub_rmd.sh
@@ -12,7 +12,7 @@ script_name=perf_seq
 memory=80000
 r_command="rusage[mem=${memory}]"
 #num_processors
-nproc=7
+nproc=3
 
 smg=/nfs/research1/marioni/alsu/singularity/R1.simg
 script=/nfs/research1/marioni/alsu/spatial/mouse_embryo/amScripts_mouseEmbryoFISH/generateData/imputation/performance/seq2atlas/run_rmd.R
