8c484eca19a9a851ae7973fc745a656e0b545f0e
amissarova <alsu.missarova.32@gmail.com>
2020-07-31 16:23:59 +0100

    add MHB genes

---
+++ generateData/imputation/imputation/getCustomGenes/brain/MHB/run_rmd.R
@@ -0,0 +1,4 @@
+library(rmarkdown)
+
+#
+render("/nfs/research1/marioni/alsu/spatial/mouse_embryo/amScripts_mouseEmbryoFISH/generateData/imputation/imputation/getCustomGenes/brain/MHB/getCustomGenes.Rmd")
