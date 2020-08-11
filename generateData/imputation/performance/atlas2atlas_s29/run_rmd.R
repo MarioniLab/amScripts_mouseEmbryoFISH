c95847fc3ece4e50d9875c11d48886dce379b257
Alsu Missarova <alsu@noah-login-02.ebi.ac.uk>
2020-08-11 13:19:15 +0100

    delete old folders

--- generateData/imputation/performance/atlas2atlas_s29/run_rmd.R
+++
@@ -1,3 +0,0 @@
-library(rmarkdown)
-
-render("/nfs/research1/marioni/alsu/spatial/mouse_embryo/amScripts_mouseEmbryoFISH/generateData/imputation/performance/atlas2atlas_s29/performance_atlas2atlas_s29.Rmd")
