6d55361a7d75da2136202745bbfd92bb06b88e9d
Alsu Missarova <alsu@noah-login-02.ebi.ac.uk>
2020-08-11 14:19:05 +0100

    delete old files

--- generateData/imputation/performance/seq2atlas/embryo2_z2/run_rmd.R
+++
@@ -1,3 +0,0 @@
-library(rmarkdown)
-
-render("/nfs/research1/marioni/alsu/spatial/mouse_embryo/amScripts_mouseEmbryoFISH/generateData/imputation/performance/seq2atlas/embryo2_z2/performance_seq2atlas.Rmd")
