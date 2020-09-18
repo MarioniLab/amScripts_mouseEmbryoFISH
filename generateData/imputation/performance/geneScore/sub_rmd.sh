61f66a79efb2df087cdc16029368eccf38ab43aa
amissarova <alsu.missarova.32@gmail.com>
2020-09-18 14:41:29 +0100

    changed to bigmem

--- generateData/imputation/performance/geneScore/sub_rmd.sh
+++ generateData/imputation/performance/geneScore/sub_rmd.sh
@@ -9,15 +9,15 @@ script_name=score
 
 #CHOOSE PARAMETERS
 #RAM in megabytes
-memory=90000
+memory=200000
 r_command="rusage[mem=${memory}]"
 #num_processors
-nproc=3
+nproc=1
 
 smg=/nfs/research1/marioni/alsu/singularity/R1.simg
 script=/nfs/research1/marioni/alsu/spatial/mouse_embryo/amScripts_mouseEmbryoFISH/generateData/imputation/performance/geneScore/run_rmd.R
 
 bsub -q research-rh74 -e ${err_folder}/${script_name} \
 -o ${out_folder}/${script_name} \
--M $memory -R $r_command -n $nproc -J ${script_name} \
+-M $memory -R $r_command -n $nproc -P bigmem -J ${script_name} \
 "singularity exec $smg Rscript $script"
