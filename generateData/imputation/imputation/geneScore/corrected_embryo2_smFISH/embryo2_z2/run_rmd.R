64306f0224aa4b9c264486a9e11f32626a7086ec
amissarova <alsu.missarova.32@gmail.com>
2020-09-28 16:26:46 +0100

    gene scores for bad FOVs

---
+++ generateData/imputation/imputation/geneScore/corrected_embryo2_smFISH/embryo2_z2/run_rmd.R
@@ -0,0 +1,3 @@
+library(rmarkdown)
+
+render("/nfs/research1/marioni/alsu/spatial/mouse_embryo/amScripts_mouseEmbryoFISH/generateData/imputation/imputation/geneScore/corrected_embryo2_smFISH/embryo2_z2/geneScore.Rmd")
