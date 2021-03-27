7c4d3893f86995115364162010dcede29a210a67
amissarova <alsu.missarova.32@gmail.com>
2021-03-27 10:34:21 +0000

    other embs

---
+++ generateData/imputation/imputation/geneScore/gene_score_filtered/embryo2_z2/run_rmd.R
@@ -0,0 +1,3 @@
+library(rmarkdown)
+
+render("/nfs/research1/marioni/alsu/spatial/mouse_embryo/amScripts_mouseEmbryoFISH/generateData/imputation/imputation/geneScore/gene_score_filtered/embryo2_z2/geneScore_filtered.Rmd")
