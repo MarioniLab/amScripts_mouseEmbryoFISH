f14ee0512f81a90b0e0df22ac19d57ba38027455
amissarova <alsu.missarova.32@gmail.com>
2021-04-14 16:50:57 +0100

    delete old

--- analysis/CT2CT/CT2CT__markers_save/run_rmd.R
+++
@@ -1,3 +0,0 @@
-library(rmarkdown)
-
-render("/nfs/research1/marioni/alsu/spatial/mouse_embryo/amScripts_mouseEmbryoFISH/analysis/CT2CT/CT2CT__markers_save/CT2CT__markers.Rmd")
--- analysis/imputation/atlas_batch/pca_hvgs/run_rmd.R
+++
@@ -1,3 +0,0 @@
-library(rmarkdown)
-
-render("/nfs/research1/marioni/alsu/spatial/mouse_embryo/amScripts_mouseEmbryoFISH/analysis/imputation/atlas_batch/pca_hvgs/pca_hvgs.Rmd")
