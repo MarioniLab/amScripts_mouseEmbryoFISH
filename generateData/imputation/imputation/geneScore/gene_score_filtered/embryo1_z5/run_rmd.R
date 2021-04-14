b518e2907642dab36f3f2ae553ecb534191437c9
amissarova <alsu.missarova.32@gmail.com>
2021-04-14 16:49:39 +0100

    delete old

--- generateData/imputation/imputation/geneScore/gene_score_filtered/embryo1_z5/run_rmd.R
+++
@@ -1,3 +0,0 @@
-library(rmarkdown)
-
-render("/nfs/research1/marioni/alsu/spatial/mouse_embryo/amScripts_mouseEmbryoFISH/generateData/imputation/imputation/geneScore/gene_score_filtered/embryo1_z5/geneScore_filtered.Rmd")
