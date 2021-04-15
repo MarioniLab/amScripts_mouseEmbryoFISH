b7b14920e1480b67adc75a163fda167e3002771d
amissarova <alsu.missarova.32@gmail.com>
2021-04-15 12:06:31 +0100

    update and standartize

--- visualization_functions.R
+++ visualization_functions.R
@@ -7,7 +7,9 @@ celltype_colours = c("Epiblast" = "#635547",
                      "Anterior Primitive Streak" = "#c19f70",
                      "Notochord" = "#0F4A9C",
                      "Def. endoderm" = "#F397C0",
+                     "Definitive endoderm" = "#F397C0",
                      "Gut" = "#EF5A9D",
+                     "Gut tube" = "#EF5A9D",
                      
                      "Nascent mesoderm" = "#C594BF",
                      "Mixed mesoderm" = "#DFCDE4",
@@ -16,9 +18,11 @@ celltype_colours = c("Epiblast" = "#635547",
                      "Paraxial mesoderm" = "#8DB5CE",
                      "Somitic mesoderm" = "#005579",
                      "Pharyngeal mesoderm" = "#C9EBFB",
+                     "Splanchnic mesoderm" = "#C9EBFB",
                      "Cardiomyocytes" = "#B51D8D",
                      "Allantois" = "#532C8A",
                      "ExE mesoderm" = "#8870ad",
+                     "Lateral plate mesoderm" = "#8870ad",
                      "Mesenchyme" = "#cc7818",
                      
                      "Haematoendothelial progenitors" = "#FBBE92",
@@ -48,7 +52,8 @@ celltype_colours = c("Epiblast" = "#635547",
                      "ExE ectoderm" = "#989898",
                      "Parietal endoderm" = "#1A1A1A",
                      
-                     "Unknown" = "#000000",
+                     "Unknown" = "#FFFFFF",
+                     "Low quality" = "#FFFFFF",
                      
                      # somitic and paraxial types
                      # colour from T chimera paper submission
@@ -60,75 +65,12 @@ celltype_colours = c("Epiblast" = "#635547",
                      "Presomitic mesoderm" = "#9DD84A"
 )
 
-stage_colours = c("E6.5" = "#D53E4F",
-                  "E6.75" = "#F46D43",
-                  "E7.0" = "#FDAE61",
-                  "E7.25" = "#FEE08B",
-                  "E7.5" = "#FFFFBF",
-                  "E7.75" = "#E6F598",
-                  "E8.0" = "#ABDDA4",
-                  "E8.25" = "#66C2A5",
-                  "E8.5" = "#3288BD",
-                  "mixed_gastrulation" = "#A9A9A9")
-stage_labels = names(stage_colours)
-names(stage_labels) = names(stage_colours)
-stage_labels[10] = "Mixed"
-
-
-scale_colour_Publication <- function(...){
-  library(scales)
-  discrete_scale("colour", "Publication",
-                 manual_pal(values = c(
-                   "#000000", "#1CE6FF", "#FF34FF", "#FF4A46", "#008941", "#006FA6", "#A30059",
-                   "#7A4900", "#0000A6", "#63FFAC", "#B79762", "#004D43", "#8FB0FF", "#997D87",
-                   "#5A0007", "#809693", "#1B4400", "#4FC601", "#3B5DFF", "#4A3B53", "#FF2F80",
-                   "#61615A", "#BA0900", "#6B7900", "#00C2A0", "#FFAA92", "#FF90C9", "#B903AA", "#D16100",
-                   "#000035", "#7B4F4B", "#A1C299", "#300018", "#0AA6D8", "#013349", "#00846F",
-                   "#372101", "#FFB500", "#A079BF", "#CC0744", "#C0B9B2", "#C2FF99", "#001E09",
-                   "#00489C", "#6F0062", "#0CBD66", "#EEC3FF", "#456D75", "#B77B68", "#7A87A1", "#788D66",
-                   "#885578", "#FAD09F", "#FF8A9A", "#D157A0", "#BEC459", "#456648", "#0086ED", "#886F4C",
-                   "#34362D", "#B4A8BD", "#00A6AA", "#452C2C", "#636375", "#A3C8C9", "#FF913F", "#938A81",
-                   "#575329", "#00FECF", "#B05B6F", "#8CD0FF", "#3B9700", "#04F757", "#C8A1A1", "#1E6E00",
-                   "#7900D7", "#A77500", "#6367A9", "#A05837", "#6B002C", "#772600", "#D790FF", "#9B9700",
-                   "#549E79", "#FFF69F", "#201625", "#72418F", "#BC23FF", "#99ADC0", "#3A2465", "#922329",
-                   "#5B4534", "#FDE8DC", "#404E55", "#0089A3", "#CB7E98", "#A4E804", "#324E72", "#6A3A4C")), ...)
-}
-
-scale_color_Publication = scale_colour_Publication
-
-#removed yellow from position 2 ("#FFFF00")
-scale_fill_Publication <- function(...){
-  library(scales)
-  discrete_scale("fill", "Publication",
-                 manual_pal(values = c(
-                   "#000000", "#1CE6FF", "#FF34FF", "#FF4A46", "#008941", "#006FA6", "#A30059",
-                   "#7A4900", "#0000A6", "#63FFAC", "#B79762", "#004D43", "#8FB0FF", "#997D87",
-                   "#5A0007", "#809693", "#1B4400", "#4FC601", "#3B5DFF", "#4A3B53", "#FF2F80",
-                   "#61615A", "#BA0900", "#6B7900", "#00C2A0", "#FFAA92", "#FF90C9", "#B903AA", "#D16100",
-                   "#000035", "#7B4F4B", "#A1C299", "#300018", "#0AA6D8", "#013349", "#00846F",
-                   "#372101", "#FFB500", "#A079BF", "#CC0744", "#C0B9B2", "#C2FF99", "#001E09",
-                   "#00489C", "#6F0062", "#0CBD66", "#EEC3FF", "#456D75", "#B77B68", "#7A87A1", "#788D66",
-                   "#885578", "#FAD09F", "#FF8A9A", "#D157A0", "#BEC459", "#456648", "#0086ED", "#886F4C",
-                   "#34362D", "#B4A8BD", "#00A6AA", "#452C2C", "#636375", "#A3C8C9", "#FF913F", "#938A81",
-                   "#575329", "#00FECF", "#B05B6F", "#8CD0FF", "#3B9700", "#04F757", "#C8A1A1", "#1E6E00",
-                   "#7900D7", "#A77500", "#6367A9", "#A05837", "#6B002C", "#772600", "#D790FF", "#9B9700",
-                   "#549E79", "#FFF69F", "#201625", "#72418F", "#BC23FF", "#99ADC0", "#3A2465", "#922329",
-                   "#5B4534", "#FDE8DC", "#404E55", "#0089A3", "#CB7E98", "#A4E804", "#324E72", "#6A3A4C")), ...)
-}
 
 gut_suborgans_colours = c("0" = "gray80" , "Lung 1" = "deeppink4", "Lung 2" = "lightpink2", "Pancreas 1" = "seagreen4", 
                           "Pancreas 2" = "darkseagreen", "Thymus" = "skyblue2", "Small intestine" = "navyblue", 
                           "Thyroid" = "darkslategrey", "Liver" = "tan2" , "Large intestine/Colon" = "lightpink4"
 )
 
-scale_color_batch_atlas <- function(...){
-  library(scales)
-  discrete_scale("colour", "Publication",
-                 manual_pal(values = c( 
-                   "black" , "tan2", "deeppink4", "skyblue2", 
-                   "darkseagreen", "navyblue","lightpink2", 
-                    "darkslategrey",  "seagreen4" , "gray80")), ...)
-}
 
                    
                    
